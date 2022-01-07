56596d43dd58928870de9bb5cb37dace2e41f3aa
naimkst <najmulspark@gmail.com>
2022-01-07 20:53:14 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+(function($) {
+  "use strict";
+
+  /*-------------------------------------------
+  Sticky Header
+  --------------------------------------------- */
+  $(window).on('scroll', function(){
+    if( $(window).scrollTop()>80 ){
+      $('#sticky').addClass('stick');
+    } else {
+      $('#sticky').removeClass('stick');
+    }
+  }); 
+  
+  jQuery(document).ready(function(){
+    /*-------------------------------------------
+    js scrollup
+    --------------------------------------------- */
+    $.scrollUp({
+      scrollText: '<i class="fa fa-angle-up"></i>',
+      easingType: 'linear',
+      scrollSpeed: 300,
+      animation: 'fade'
+    }); 
+
+    /*-------------------------------------------
+    js onePageNav
+    --------------------------------------------- */
+    $('.toggle-bar').on('click', function () {
+      $('.menu-area').slideToggle();
+    });
+
+  });
+
+})(jQuery);
